23a7d7cdab850cf6e316c1e1d57257042d48bd2a
ADRIANA AVALOS VARGAS <avalos_84@hotmail.com>
2020-11-02 21:41:46 -0600

    ain

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,18 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+//Select the table
+var ufoTable = d3.select("table")
+//Select the table body
+var tbody = ufoTable.select("tbody")
+
+//Render the table without filter
+data.forEach((data) => {
+    var row = tbody.append("tr");
+    Object.entries(data).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+
